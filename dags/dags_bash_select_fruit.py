49773024bd3eb28dad4363f2bf1039099e9d177e
휘래 김 <hramst0618@gmail.com>
2025-07-15 15:49:52 +0900

    select_fruit.sh 수행하는 dag 업로드

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -6,7 +6,7 @@ from airflow.providers.standard.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_select_fruit",
     schedule="10 0 * * 6#1",
-    start_date=pendulum.datetime(2025, 7, 1, tz="Asia.Seoul"),
+    start_date=pendulum.datetime(2025, 7, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     
