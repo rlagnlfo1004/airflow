2b3dc225874ae6374093b15a5414f76da68db8be
휘래 김 <hramst0618@gmail.com>
2025-07-17 16:51:59 +0900

    task decorator 업로드

--- dags/dags_python_decorator.py
+++ dags/dags_python_decorator.py
@@ -1,18 +1,15 @@
 from airflow.sdk import DAG
 import pendulum
-import datetime
-from airflow.providers.standard.operators.python import PythonOperator
-from airflow.sdk.definitions.decorators import task
-
+from airflow.decorators import task
 with DAG(
-    dag_id ="dags_python_decorator",
-    schedule = "0 2 * * 1",
-    start_date=pendulum.datetime(2025, 7, 1, tz='Asia/Seoul'),
-    catchup=False,
+    dag_id = "dags_python_decorator",
+    schedule="0 2 * * *",
+    start_date=pendulum.datetime(2025, 7, 1, tz="Asia/Seoul"),
+    catchup=False
 ) as dag:
 
     @task(task_id="python_task_1")
-    def print_context(some_input):
+    def python_context(some_input):
         print(some_input)
 
-    python_task_1 = print_context('task_decorator 실행')
+    python_task_1 = python_context("task_decorator 실행")
