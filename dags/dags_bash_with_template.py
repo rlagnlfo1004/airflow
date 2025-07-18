1204ecf06ed8271b3cb8c75bfc2f6f8ca04aeb30
휘래 김 <hramst0618@gmail.com>
2025-07-19 01:19:40 +0900

    bash_template dag 추가

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -10,16 +10,16 @@ with DAG(
 ) as dag:
     bash_t1 = BashOperator(
         task_id='bash_t1',
-        bash_command='echo "data_interval_end: {{ data_interval_end }} "'
+        bash_command='echo data_interval_end: {{ data_interval_end }} '
     )
 
     bash_t2= BashOperator(
         task_id='bash_t2',
         env={
-            'SART_DATE': '{{ data_interval_end | ds }}',
+            'START_DATE': '{{ data_interval_end | ds }}',
             'END_DATE' : '{{ data_interval_end | ds }}',
         },
-        bash_command='echo $SART_DATE && echo $END_DATE'
+        bash_command='echo $START_DATE && echo $END_DATE'
     )
 
     bash_t1 >> bash_t2
