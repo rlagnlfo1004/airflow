36f56b2ed8be02e739c39b845ae0acd9ad931622
휘래 김 <hramst0618@gmail.com>
2025-07-19 00:17:01 +0900

    op_kwargs 추가

---
+++ dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,20 @@
+import pendulum
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import DAG
+from common.common_func import regist2
+
+with DAG(
+    dag_id = "dags_python_with_op_kwargs",
+    schedule = " 30 6 * * *",
+    start_date=pendulum.datetime(2025, 7, 1, tz="Asia/Seoul"),
+    catchup = False
+) as dag :
+
+    regist2_t1 = PythonOperator(
+        task_id = "regist2_t1",
+        python_callable =regist2,
+        op_args=['hrkim', 'man', 'kr', 'seoul'],
+        op_kwargs={"email": "hramst0618@gmail.com", "phone": "010-1234-5678"}
+    )
+
+    regist2_t1
