0ea24c690b17c7a0f7b40e712ed7512bb536d3e3
휘래 김 <hramst0618@gmail.com>
2025-07-27 22:53:06 +0900

    kwargs 확인

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+import pendulum
+from airflow.decorators import task
+
+with DAG(
+    dag_id = 'dags_python_show_templates',
+    schedule = "30 9 * * *",
+    start_date = pendulum.datetime(2025, 7, 24, tz='Aisa/Seoul'),
+    catchup=True
+)as dag:
+
+    @task(task_id = "python_task")
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_templates()
