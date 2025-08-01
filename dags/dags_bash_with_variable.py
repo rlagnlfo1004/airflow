5cba434b019275a86267028cc5cb0c820c7c1077
휘래 김 <hramst0618@gmail.com>
2025-08-02 00:50:10 +0900

    전역변수 Variable 사용

---
+++ dags/dags_bash_with_variable.py
@@ -0,0 +1,23 @@
+import pendulum
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.sdk import DAG, Variable
+
+with DAG(
+    dag_id='dags_bash_with_variable',
+    schedule='10 0 * * *',
+    start_date=pendulum.datetime(2025, 7, 1, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+
+    var_value=Variable.get("var_value")
+    bash_var_1 = BashOperator(
+        task_id='bash_var_1',
+        bash_command=f'echo variable:{var_value}'
+    )
+
+    bash_var_2 = BashOperator(
+        task_id='bash_var_2',
+        bash_command="echo variable: {{ var.value.sample_key }}"
+    )
+
+    bash_var_1 >> bash_var_2
