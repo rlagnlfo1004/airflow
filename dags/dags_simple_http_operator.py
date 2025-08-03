ad99725135f11ed88fc685204d6977a126bf78f4
휘래 김 <hramst0618@gmail.com>
2025-08-03 22:16:36 +0900

    HttpOperator를 통한 서울시 공공 데이터 가져오기

--- dags/dags_simple_http_operator.py
+++ dags/dags_simple_http_operator.py
@@ -13,7 +13,7 @@ with DAG(
 
     tb_cycle_station_info = HttpOperator(
         task_id='tb_cycle_station_info',
-        http_conn_id='openapi.seoul.go.kr',
+        http_conn_id='openapi-seoul-data',
         endpoint='{{ var.value.apikey_openapi_seoul_go_kr }}/json/tbCycleStationInfo/1/10/',
         method='GET',
         headers={'Content-Type': 'application/json',
