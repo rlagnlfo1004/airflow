ba5aa4532b026cb5daeb1c55c7bf8b0cab188959
휘래 김 <hramst0618@gmail.com>
2025-08-06 02:03:03 +0900

    Hook 이용하여 Postgres Insert

---
+++ dags/dags_python_with_postgres_hook.py
@@ -0,0 +1,34 @@
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import DAG
+import pendulum
+
+with DAG(
+    dag_id='dags_python_with_postgres_hook',
+    start_date=pendulum.datetime(2025, 8, 1, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+)as dag :
+
+    def insrt_postgres(postgres_conn_id, **kwargs):
+        from airflow.providers.postgres.hooks.postgres import PostgresHook
+        from contextlib import closing
+
+        postgres_hook = PostgresHook(postgres_conn_id)
+        with closing(PostgresHook.get_conn()) as conn:
+            with closing(conn.cursor()) as cursor:
+                dag_id = kwargs.get('ti').dag_id
+                task_id = kwargs.get('ti').task_id
+                run_id = kwargs.get('ti').run_id
+                msg = 'hook inset 수행'
+                sql = 'insert into py_opr_drct_inset values (%s, %s, %s, %s);'
+
+                cursor.execute(sql, (dag_id, task_id, run_id, msg))
+                conn.commit()
+
+    insrt_postgres = PythonOperator(
+        task_id = 'insrt_postgres',
+        python_callable=insrt_postgres,
+        op_kwargs={'postgres_conn_id': 'conn-db-postgres-cuustom'}
+    )
+
+    insrt_postgres
