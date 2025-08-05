479cc02f851bb4c143cb94ca73a4316445feaff2
휘래 김 <hramst0618@gmail.com>
2025-08-06 02:06:42 +0900

    Hook 이용하여 Postgres Insert

--- dags/dags_python_with_postgres_hook.py
+++ dags/dags_python_with_postgres_hook.py
@@ -28,7 +28,7 @@ with DAG(
     insrt_postgres = PythonOperator(
         task_id = 'insrt_postgres',
         python_callable=insrt_postgres,
-        op_kwargs={'postgres_conn_id': 'conn-db-postgres-cuustom'}
+        op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom'}
     )
 
     insrt_postgres
