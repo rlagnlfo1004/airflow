1232510cb9cdcb8e95ad279649aa5a58a866b182
휘래 김 <hramst0618@gmail.com>
2025-08-06 00:50:42 +0900

    postgres에 데이터  insert

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -8,19 +8,16 @@ with DAG(
     start_date=pendulum.datetime(2025, 7, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-    
-    # [START howto_operator_bash]
-    
+
     bash_t1 = BashOperator(
         task_id="bash_t1",
         bash_command="echo whoami",
     )
-    
+
     bash_t2 = BashOperator(
         task_id="bash_t2",
         bash_command="echo $HOSTNAME",
     )
-    
-    # [END howto_operator_bash]
-    
+
     bash_t1 >> bash_t2
+
