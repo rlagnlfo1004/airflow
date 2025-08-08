9130eadfbac96d7abc0a75e104842c498bd2dc44
휘래 김 <hramst0618@gmail.com>
2025-08-08 18:39:49 +0900

    Custom 훅 정의

--- plugins/hooks/custom_postgres_hook.py
+++ plugins/hooks/custom_postgres_hook.py
@@ -8,17 +8,8 @@ class CustomPostgresHook(BaseHook):
     def __init__(self, postgres_conn_id, **kwargs):
         super().__init__(**kwargs)
         self.postgres_conn_id = postgres_conn_id
-        self.airflow_conn = BaseHook.get_connection(self.postgres_conn_id)
-
-        self.host = self.airflow_conn.host
-        self.user = self.airflow_conn.login
-        self.password = self.airflow_conn.password
-        self.dbname = self.airflow_conn.schema
-        self.port = self.airflow_conn.port
-        self.uri = f'postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.dbname}'
-
-    def get_sqlalchemy_engine(self):
-        return create_engine(self.uri)
+        conn = BaseHook.get_connection(self.postgres_conn_id)
+        self.uri = f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}'
 
     def bulk_load(self, table_name, file_name, delimiter: str, is_header: bool, is_replace: bool):
         self.log.info('적재 대상파일:' + file_name)
@@ -26,18 +17,20 @@ class CustomPostgresHook(BaseHook):
 
         header = 0 if is_header else None
         if_exists = 'replace' if is_replace else 'append'
+
         file_df = pd.read_csv(file_name, header=header, delimiter=delimiter)
 
         for col in file_df.columns:
-            try:
-                # string 문자열이 아닐 경우 continue
-                file_df[col] = file_df[col].str.replace('\r\n', '')  # 줄넘김 및 ^M 제거
+            if file_df[col].dtype == 'object':
+                file_df[col] = file_df[col].astype(str).str.replace('\r\n', '', regex=True)
                 self.log.info(f'{table_name}.{col}: 개행문자 제거')
-            except:
-                continue
 
         self.log.info('적재 건수:' + str(len(file_df)))
-        engine = self.get_sqlalchemy_engine()
+
+        # SQLAlchemy Engine 생성
+        engine = create_engine(self.uri)
+
+        # 'con' 인수에 engine 객체 전달
         file_df.to_sql(name=table_name,
                        con=engine,
                        schema='public',
