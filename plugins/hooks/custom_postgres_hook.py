d040c74ebefbf615c84b41921836d6160de41c24
휘래 김 <hramst0618@gmail.com>
2025-08-08 17:59:32 +0900

    Custom 훅 정의

--- plugins/hooks/custom_postgres_hook.py
+++ plugins/hooks/custom_postgres_hook.py
@@ -41,10 +41,9 @@ class CustomPostgresHook(BaseHook):
         self.log.info('적재 건수:' + str(len(file_df)))
         uri = f'postgresql://{self.user}:{self.password}@{self.host}/{self.dbname}'
         engine = create_engine(uri)
-        file_df.to_sql(
-            name=table_name,
-            con=engine,
-            schema='public',
-            if_exists=if_exists,
-            index=False
-        )
+        file_df.to_sql(name=table_name,
+                       con=engine,
+                       schema='public',
+                       if_exists=if_exists,
+                       index=False
+                       )
