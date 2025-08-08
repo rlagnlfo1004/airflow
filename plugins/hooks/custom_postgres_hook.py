d5f318c66a28e33c5e3b0e9b96039d9c7f625317
휘래 김 <hramst0618@gmail.com>
2025-08-08 18:28:16 +0900

    Custom 훅 정의

--- plugins/hooks/custom_postgres_hook.py
+++ plugins/hooks/custom_postgres_hook.py
@@ -1,49 +1,49 @@
 from airflow.hooks.base import BaseHook
-import psycopg2
 import pandas as pd
+from sqlalchemy import create_engine
 
 
 class CustomPostgresHook(BaseHook):
 
     def __init__(self, postgres_conn_id, **kwargs):
+        super().__init__(**kwargs)
         self.postgres_conn_id = postgres_conn_id
+        self.airflow_conn = BaseHook.get_connection(self.postgres_conn_id)
 
-    def get_conn(self):
-        airflow_conn = BaseHook.get_connection(self.postgres_conn_id)
-        self.host = airflow_conn.host
-        self.user = airflow_conn.login
-        self.password = airflow_conn.password
-        self.dbname = airflow_conn.schema
-        self.port = airflow_conn.port
+        self.host = self.airflow_conn.host
+        self.user = self.airflow_conn.login
+        self.password = self.airflow_conn.password
+        self.dbname = self.airflow_conn.schema
+        self.port = self.airflow_conn.port
+        self.uri = f'postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.dbname}'
 
-        self.postgres_conn = psycopg2.connect(host=self.host, user=self.user, password=self.password,
-                                              dbname=self.dbname, port=self.port)
-        return self.postgres_conn
+    def get_sqlalchemy_engine(self):
+        return create_engine(self.uri)
 
     def bulk_load(self, table_name, file_name, delimiter: str, is_header: bool, is_replace: bool):
-        from sqlalchemy import create_engine
-
         self.log.info('적재 대상파일:' + file_name)
         self.log.info('테이블 :' + table_name)
-        self.get_conn()
-        header = 0 if is_header else None  # is_header = True면 0, False면 None
-        if_exists = 'replace' if is_replace else 'append'  # is_replace = True면 replace, False면 append
-        file_df = pd.read_csv(file_name, header=header, delimiter=delimiter)
+
+        header = 0 if is_header else None
+        if_exists = 'replace' if is_replace else 'append'
+
+        try:
+            file_df = pd.read_csv(file_name, header=header, delimiter=delimiter, encoding='utf-8')
+        except UnicodeDecodeError:
+            file_df = pd.read_csv(file_name, header=header, delimiter=delimiter, encoding='cp949')
 
         for col in file_df.columns:
-            try:
-                # string 문자열이 아닐 경우 continue
-                file_df[col] = file_df[col].str.replace('\r\n', '')  # 줄넘김 및 ^M 제거
-                self.log.info(f'{table_name}.{col}: 개행문자 제거')
-            except:
-                continue
+            if file_df[col].dtype == 'object':
+                file_df[col] = file_df[col].astype(str).str.replace(r'[\r\n\t]+', '', regex=True)
+                self.log.info(f'{table_name}.{col}: 개행문자, 탭 제거')
 
         self.log.info('적재 건수:' + str(len(file_df)))
-        uri = f'postgresql://{self.user}:{self.password}@{self.host}/{self.dbname}'
-        engine = create_engine(uri)
+
+        engine = self.get_sqlalchemy_engine()
+
         file_df.to_sql(name=table_name,
                        con=engine,
                        schema='public',
                        if_exists=if_exists,
-                       index=False
+                       index=False,
                        )
