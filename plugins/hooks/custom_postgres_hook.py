767cc223061b21db266e9432d034dde1e2886188
휘래 김 <hramst0618@gmail.com>
2025-08-08 18:48:23 +0900

    Custom 훅 정의

--- plugins/hooks/custom_postgres_hook.py
+++ plugins/hooks/custom_postgres_hook.py
@@ -8,6 +8,7 @@ class CustomPostgresHook(BaseHook):
     def __init__(self, postgres_conn_id, **kwargs):
         super().__init__(**kwargs)
         self.postgres_conn_id = postgres_conn_id
+        # 에어플로우 커넥션을 가져옵니다.
         self.airflow_conn = BaseHook.get_connection(self.postgres_conn_id)
 
         self.host = self.airflow_conn.host
@@ -15,9 +16,14 @@ class CustomPostgresHook(BaseHook):
         self.password = self.airflow_conn.password
         self.dbname = self.airflow_conn.schema
         self.port = self.airflow_conn.port
+
+        # SQLAlchemy URI를 생성합니다.
         self.uri = f'postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.dbname}'
 
+    # get_conn() 메서드를 삭제합니다.
+
     def get_sqlalchemy_engine(self):
+        # SQLAlchemy Engine 객체를 반환하는 메서드입니다.
         return create_engine(self.uri)
 
     def bulk_load(self, table_name, file_name, delimiter: str, is_header: bool, is_replace: bool):
@@ -26,19 +32,25 @@ class CustomPostgresHook(BaseHook):
 
         header = 0 if is_header else None
         if_exists = 'replace' if is_replace else 'append'
-        file_df = pd.read_csv(file_name, header=header, delimiter=delimiter)
+
+        # CSV 파일을 pandas DataFrame으로 읽어옵니다.
+        try:
+            file_df = pd.read_csv(file_name, header=header, delimiter=delimiter, encoding='utf-8')
+        except UnicodeDecodeError:
+            file_df = pd.read_csv(file_name, header=header, delimiter=delimiter, encoding='cp949')
+
         for col in file_df.columns:
-            try:
-                # string 문자열이 아닐 경우 continue
-                file_df[col] = file_df[col].str.replace('\r\n', '')  # 줄넘김 및 ^M 제거
-                self.log.info(f'{table_name}.{col}: 개행문자 제거')
-            except:
-                continue
+            if file_df[col].dtype == 'object':
+                # 문자열 열의 개행 문자, 탭 등을 제거합니다.
+                file_df[col] = file_df[col].astype(str).str.replace(r'[\r\n\t]+', '', regex=True)
+                self.log.info(f'{table_name}.{col}: 개행문자, 탭 제거')
 
         self.log.info('적재 건수:' + str(len(file_df)))
 
+        # SQLAlchemy Engine 객체를 생성합니다.
         engine = self.get_sqlalchemy_engine()
 
+        # pandas.to_sql에 engine 객체를 전달하여 데이터를 적재합니다.
         file_df.to_sql(name=table_name,
                        con=engine,
                        schema='public',
