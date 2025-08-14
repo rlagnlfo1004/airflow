dfe22fa8f6c6ca4133d62ce57b1bd583730eb1ca
휘래 김 <hramst0618@gmail.com>
2025-08-14 15:26:17 +0900

    EmailOperator를 활용한 smtp Gmail 전송

---
+++ dags/dags_email_operator.py
@@ -0,0 +1,18 @@
+import pendulum
+from airflow.sdk import DAG
+from airflow.providers.smtp.operators.smtp import EmailOperator
+
+with DAG(
+    dag_id='dags_email_operator',
+    schedule='0 8 1 * *',
+    start_date=pendulum.datetime(2025, 8, 1, tz='Asia/Seoul'),
+    catchup=False,
+) as dag:
+
+    send_email_task = EmailOperator(
+        task_id='send_email_task',
+        conn_id='conn_smtp_gmail',
+        to='hrkim2001@ajou.ac.kr',
+        subject='Airflow 성공 메일',
+        html_content='Airflow DAG가 성공적으로 실행되었습니다.',
+    )
